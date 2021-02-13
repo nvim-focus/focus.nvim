65d1375c2580f0b9c5abbcca2033e55e9c21f985
beauwilliams <beau.williams@icloud.com>
2021-02-14 09:59:22 +1100

    Adding height configuration

--- lua/focus.lua
+++ lua/focus.lua
@@ -1,10 +1,12 @@
 local M = {}
 
-M.focus = true -- Default to true
-M.width = 50
+M.focus = true -- Default to enabled
+M.height_compatible = false
+M.width = 120 --> Default to 120 for now
+M.height = 0 --> Don't set height by default for now
 M.focus_init = function()
     if M.focus == true then
-    require 'modules.autocmd'.setup(M.width)
+    require 'modules.autocmd'.setup(M.width,M.height)
     end
 end
 
--- lua/modules/resizer.lua
+++ lua/modules/resizer.lua
@@ -1,15 +1,17 @@
 local vim = vim --> Use locals
 
 local M = {}
-function M.split_resizer(width) --> Only resize normal buffers, set qf to 10 always
+function M.split_resizer(width,height) --> Only resize normal buffers, set qf to 10 always
     if vim.api.nvim_eval('g:focus_enabled') == 0 then return end
     if vim.bo.ft == 'NvimTree' or vim.bo.ft == 'NerdTree'  or vim.bo.ft == 'CHADTree' then return end
     if vim.bo.ft == 'qf' then
         vim.o.winwidth = 10
     else
-        vim.o.winwidth = width
-        vim.cmd('wincmd=') --> Auto-Maximize windows horizontally when in focus
+        vim.o.winwidth = width --> lua print(vim.o.winwidth)
     end
+    if height ~= 0 then vim.o.winheight = height --> Opt in to set height value, otherwise auto-size it
+    else vim.cmd('wincmd=') end--> Auto-Maximize windows horizontally when in focus by DEFAULT
+
 
 end
 
