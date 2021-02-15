e5d489d702d6646cad7669cbe719af7020b571cd
beauwilliams <beau.williams@icloud.com>
2021-02-16 01:10:40 +1100

    Rafactor: focus to enable. FIX:#3

--- lua/focus.lua
+++ lua/focus.lua
@@ -1,13 +1,13 @@
 local M = {}
 
-M.focus = true -- Default to enabled
+M.enable = true -- Default to enabled
 M.height_compatible = false
 M.width = 120 --> Default to 120 for now
 M.height = 0 --> Don't set height by default for now
 M.focus_init = function()
     if M.width == nil then M.width = 120 end --> Catch exceptions in case people set values to nil in their init.lua
     if M.height == nil then M.height = 0 end
-    if M.focus == true then
+    if M.enable == true then
     require 'modules.autocmd'.setup(M.width,M.height)
     end
 end
