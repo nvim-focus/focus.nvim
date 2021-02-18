8a6824940bddd51e778a23720ecfeb00d4051f47
beauwilliams <beau.williams@icloud.com>
2021-02-18 18:38:26 +1100

    REFACTOR: Remove exception for telescope no longer needed

--- lua/modules/resizer.lua
+++ lua/modules/resizer.lua
@@ -4,7 +4,6 @@ local M = {}
 function M.split_resizer(width,height) --> Only resize normal buffers, set qf to 10 always
     if vim.api.nvim_eval('g:enabled_focus') == 0 then return end
     if vim.bo.ft == 'NvimTree' or vim.bo.ft == 'NerdTree'  or vim.bo.ft == 'CHADTree' then return end
-    if vim.bo.ft == 'TelescopePrompt' then vim.o.winheight=1 end
     if vim.bo.ft == 'qf' then
         vim.o.winwidth = 10
     else
