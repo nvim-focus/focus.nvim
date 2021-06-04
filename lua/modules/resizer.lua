35d3f229e2e6a724ff86950df2289e071fb91a96
beauwilliams <beau.williams@icloud.com>
2021-06-04 18:10:52 +1000

    Adding temporary fix for #15 using autocommand and some vimscript. Might need rethink how sizes are applied for resizer.lua to work

--- lua/modules/resizer.lua
+++ lua/modules/resizer.lua
@@ -1,11 +1,9 @@
 local vim = vim --> Use locals
-local eval = vim.api.nvim_eval
-local ft = vim.bo.ft
 
 local M = {}
 function M.split_resizer(config) --> Only resize normal buffers, set qf to 10 always
-    if eval('g:enabled_focus') == 0 then return end
-    if ft == 'NvimTree' or ft == 'NerdTree'  or ft == 'CHADTree' then return end
+    local ft = vim.bo.ft
+    -- if ft == 'NvimTree' or ft == 'NerdTree'  or ft == 'CHADTree' then FIX: This does not work
     if ft == 'qf' then
         vim.o.winwidth = 10
     else
--- plugin/focus.vim
+++ plugin/focus.vim
@@ -45,6 +45,7 @@ endif
 endfunction
 
 
+au BufHidden * if &ft=="Nvimtree" || &ft=="NerdTree" || &ft=="CHADTree" | vertical resize 35 | endif
 
 "don't require our files until we need them, hopefully it prevents lua file being
 "loaded if we run vim with focus disabled i.e `nvim +DisableFocus`
