94a308a2f94be7dd2973934581f80dd8f2516b4c
beauwilliams <beau.williams@icloud.com>
2021-09-08 23:03:38 +1000

    fix bug with focusdisable not working when the commands are set after checking it focus.enable = true

--- lua/focus/init.lua
+++ lua/focus/init.lua
@@ -6,11 +6,11 @@ local M = {}
 M.init = function()
 	-- Verify that configuration values are of the correct type
 	config.verify()
+	commands.setup() -- TODO: Prevent loading commands when enable = false, but still make sure focusdisable command works etc
 
 	if M.enable == true then
 		-- Pass this module, noting that `__index` actually references the
 		-- configuration module, to setup the autocmds used for this plugin
-		commands.setup()
 		require('focus.modules.autocmd').setup(M)
 		require('focus.modules.resizer').split_resizer(M)
 
