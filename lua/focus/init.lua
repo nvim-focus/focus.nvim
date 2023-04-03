493a9daa2fb6590003ce17670a47946709339981
Andreas Schneider <asn@cryptomilk.org>
2023-04-03 15:02:12 +0200

    refactor(config): Rewrite config parsing

--- lua/focus/init.lua
+++ lua/focus/init.lua
@@ -1,141 +1,227 @@
-local config = require('focus.modules.config')
+--- *focus.nvim* Have a nice view over your split windows
+--- *Focus*
+---
+--- MIT License
+---
+--- Copyright (c) Beau Williams
+--- Copyright (c) Andreas Schneider <asn@cryptomilk.org>
+---
+--- ===========================================================================
+
+-- Module definition ==========================================================
 local commands = require('focus.modules.commands')
 local autocmd = require('focus.modules.autocmd')
 local split = require('focus.modules.split')
 local functions = require('focus.modules.functions')
 local resizer = require('focus.modules.resizer')
-local utils = require('focus.modules.utils')
 
-local M = {}
+local H = {}
+local Focus = {}
 
-M.setup = function(options)
-	--load the configs
-	setmetatable(M, {
-		__newindex = config.set,
-		__index = config.get,
-	})
-	-- if options provided to setup, override defaults
-	if options ~= nil then
-		for k, v1 in pairs(options) do
-			config.defaults[k] = v1
-		end
-	end
-	-- Verify that configuration values are of the correct type
-	config.verify()
+Focus.config = {
+    enable = true, -- Enable module
+    commands = true, -- Create Focus commands
+    autoresize = true, -- Enable or disable auto-resizing of splits
+    width = 0, -- Force width for the focused window
+    height = 0, -- Force height for the focused window
+    minwidth = 0, -- Force minimum width for the unfocused window
+    minheight = 0, -- Force minimum height for the unfocused window
+    height_quickfix = 10, -- Set the height of quickfix panel
+    cursorline = true, -- Display a cursorline in the focussed window only
+    cursorcolumn = false, -- Display cursorcolumn in the focussed window only
+    signcolumn = true, -- Display signcolumn in the focussed window only
+    colorcolumn = {
+        enable = false, -- Display colorcolumn in the foccused window only
+        width = 80, -- Set the width for the colorcolumn
+    },
+    winhighlight = false, -- Auto highlighting for focussed/unfocussed windows
+    number = false, -- Display line numbers in the focussed window only
+    relativenumber = false, -- Display relative line numbers in the focussed window only
+    hybridnumber = false, -- Display hybrid line numbers in the focussed window only
+    absolutenumber_unfocussed = false, -- Preserve absolute numbers in the unfocussed windows
+    tmux = false, -- Create tmux splits instead of neovim splits
+    bufnew = false, -- Create blank buffer for new split windows
+}
 
-	-- Don't set up focus if its not enabled by the user
-	if M.enable then
-		-- Focus is enabled when setup is run, we use this var to enable/disable/toggle
-		vim.g.enabled_focus_resizing = 1
-		-- Pass this module M, noting that `__index` actually references the
-		-- configuration module, to setup the autocmds used for this plugin
-		autocmd.setup(M)
-		if M.commands then
-			commands.setup()
-		end
+--- Module setup
+---
+---@param config table|nil Module config table. See |Focus.config|.
+---
+---@usage `require('focus').setup({})` (replace `{}` with your `config` table)
+Focus.setup = function(config)
+    -- Export module
+    _G.Focus = Focus
 
-		if M.winhighlight then
-			if vim.fn.has('nvim-0.9') then
-				vim.api.nvim_set_hl(0, 'FocusedWindow', { link = 'VertSplit'})
-				vim.api.nvim_set_hl(0, 'UnfocusedWindow', { link = 'Normal'})
-			else
-				vim.highlight.link('FocusedWindow', 'VertSplit', true)
-				vim.highlight.link('UnfocusedWindow', 'Normal', true)
-			end
+    -- Setup config
+    config = H.setup_config(config)
 
-			vim.wo.winhighlight = 'Normal:FocusedWindow,NormalNC:UnfocusedWindow'
-		end
+    -- Apply config
+    H.apply_config(config)
 
-		-- Finally begin resizing when enabled and configs set
-		if config.autoresize then
-			M.resize()
-		end
-	end
+    if config ~= nil and config.enable then
+        autocmd.setup(config)
+
+        if config.commands then
+            commands.setup()
+        end
+
+        if config.winhighlight then
+            if vim.fn.has('nvim-0.9') then
+                vim.api.nvim_set_hl(0, 'FocusedWindow', { link = 'VertSplit' })
+                vim.api.nvim_set_hl(0, 'UnfocusedWindow', { link = 'Normal' })
+            else
+                vim.highlight.link('FocusedWindow', 'VertSplit', true)
+                vim.highlight.link('UnfocusedWindow', 'Normal', true)
+            end
+
+            vim.wo.winhighlight =
+                'Normal:FocusedWindow,NormalNC:UnfocusedWindow'
+        end
+
+        if config.autoresize then
+            Focus.resize()
+        end
+    end
 end
 
-M.resize = function()
-	if utils.is_disabled() then
-			return
-	end
-	resizer.split_resizer(M)
+Focus.resize = function()
+    local config = H.get_config()
+
+    resizer.split_resizer(config)
 end
 
 -- Exported internal functions for use in commands
-function M.split_nicely(args)
-	if args == nil then
-		args = ''
-	end
-	split.split_nicely(args, M.bufnew)
+function Focus.split_nicely(args)
+    local config = H.get_config()
+    if args == nil then
+        args = ''
+    end
+    split.split_nicely(args, config.bufnew)
 end
 
-function M.split_command(direction, args)
-	args = args or ''
-	split.split_command(direction, args, M.tmux, M.bufnew)
+function Focus.split_command(direction, args)
+    local config = H.get_config()
+    args = args or ''
+    split.split_command(direction, args, config.tmux, config.bufnew)
 end
 
-function M.split_cycle(reverse)
-	split.split_cycle(reverse, M.bufnew)
+function Focus.split_cycle(reverse)
+    local config = H.get_config()
+    split.split_cycle(reverse, config.bufnew)
 end
 
-function M.focus_enable()
-	functions.focus_enable()
+function Focus.focus_enable()
+    functions.focus_enable()
 end
 
-function M.focus_disable()
-	functions.focus_disable()
+function Focus.focus_disable()
+    functions.focus_disable()
 end
 
-function M.focus_toggle()
-	functions.focus_toggle()
+function Focus.focus_toggle()
+    functions.focus_toggle()
 end
 
-function M.focus_maximise()
-	functions.focus_maximise()
+function Focus.focus_maximise()
+    functions.focus_maximise()
 end
 
-function M.focus_equalise()
-	functions.focus_equalise()
+function Focus.focus_equalise()
+    functions.focus_equalise()
 end
 
-function M.focus_max_or_equal()
-	functions.focus_max_or_equal()
+function Focus.focus_max_or_equal()
+    functions.focus_max_or_equal()
 end
 
-function M.focus_disable_window()
-	table.insert(M.excluded_windows, vim.api.nvim_get_current_win())
+function Focus.focus_disable_window()
+    table.insert(Focus.excluded_windows, vim.api.nvim_get_current_win())
 end
 
-function M.focus_enable_window()
-	for k, v in pairs(M.excluded_windows) do
-		if v == vim.api.nvim_get_current_win() then
-			table.remove(M.excluded_windows, k)
-		end
-	end
+function Focus.focus_enable_window()
+    for k, v in pairs(Focus.excluded_windows) do
+        if v == vim.api.nvim_get_current_win() then
+            table.remove(Focus.excluded_windows, k)
+        end
+    end
 end
 
-function M.focus_toggle_window()
-	for _, v in pairs(M.excluded_windows) do
-		if v == vim.api.nvim_get_current_win() then
-			M.focus_enable_window()
-			return
-		end
-	end
-	M.focus_disable_window()
+function Focus.focus_toggle_window()
+    for _, v in pairs(Focus.excluded_windows) do
+        if v == vim.api.nvim_get_current_win() then
+            Focus.focus_enable_window()
+            return
+        end
+    end
+    Focus.focus_disable_window()
 end
 
-function M.focus_get_disabled_windows()
-	print('------------------')
-	print('█▀ ▄▀▄ ▄▀▀ █ █ ▄▀▀')
-	print('█▀ ▀▄▀ ▀▄▄ ▀▄█ ▄██')
-	print('------------------')
-	print('Disabled Windows')
-	for _, v in pairs(M.excluded_windows) do
-		print('- ' .. v)
-	end
-	print('-------------------')
-	print('Current Window')
-	print('- ' .. vim.api.nvim_get_current_win())
-	print('-------------------')
+function Focus.focus_get_disabled_windows()
+    print('------------------')
+    print('█▀ ▄▀▄ ▄▀▀ █ █ ▄▀▀')
+    print('█▀ ▀▄▀ ▀▄▄ ▀▄█ ▄██')
+    print('------------------')
+    print('Disabled Windows')
+    for _, v in pairs(Focus.excluded_windows) do
+        print('- ' .. v)
+    end
+    print('-------------------')
+    print('Current Window')
+    print('- ' .. vim.api.nvim_get_current_win())
+    print('-------------------')
 end
 
-return M
+H.default_config = Focus.config
+
+H.setup_config = function(config)
+    vim.validate({ config = { config, 'table', true } })
+
+    config = vim.tbl_deep_extend('force', H.default_config, config or {})
+
+    vim.validate({
+        enable = { config.enable, 'boolean' },
+        commands = { config.commands, 'boolean' },
+        autoresize = { config.autoresize, 'boolean' },
+        width = { config.width, 'number' },
+        height = { config.height, 'number' },
+        minwidth = { config.minwidth, 'number' },
+        minheight = { config.minheight, 'number' },
+        height_quickfix = { config.height_quickfix, 'number' },
+        cursorline = { config.cursorline, 'boolean' },
+        cursorcolumn = { config.cursorcolumn, 'boolean' },
+        signcolumn = { config.signcolumn, 'boolean' },
+        colorcolumn = { config.colorcolumn, 'table', true },
+        winhighlight = { config.winhighlight, 'boolean' },
+        number = { config.number, 'boolean' },
+        relativenumber = { config.relativenumber, 'boolean' },
+        hybridnumber = { config.hybridnumber, 'boolean' },
+        absolutenumber_unfocussed = {
+            config.absolutenumber_unfocussed,
+            'boolean',
+        },
+        tmux = { config.tmux, 'boolean' },
+        bufnew = { config.bufnew, 'boolean' },
+    })
+
+    vim.validate({
+        ['colorcolumn.enable'] = { config.colorcolumn.enable, 'boolean' },
+        ['colorcolumn.width'] = { config.colorcolumn.width, 'number' },
+    })
+
+    return config
+end
+
+H.apply_config = function(config)
+    Focus.config = config
+end
+
+H.get_config = function(config)
+    return vim.tbl_deep_extend(
+        'force',
+        Focus.config,
+        vim.b.focus_config or {},
+        config or {}
+    )
+end
+
+return Focus
