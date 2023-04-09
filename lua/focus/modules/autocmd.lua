dc96b34e22ecea1f9713fcbeeb8d108f33df920a
Andreas Schneider <asn@cryptomilk.org>
2023-04-09 11:49:13 +0200

    feat(autoresize): Center the prev win when switching between horizontal splits

--- lua/focus/modules/autocmd.lua
+++ lua/focus/modules/autocmd.lua
@@ -1,3 +1,12 @@
+-- *focus.nvim* Have a nice view over your split windows
+--
+-- MIT License
+--
+-- Copyright (c) Beau Williams
+-- Copyright (c) Andreas Schneider <asn@cryptomilk.org>
+--
+-- ============================================================================
+
 local utils = require('focus.modules.utils')
 local vim = vim
 local M = {}
@@ -21,16 +30,57 @@ function M.setup(config)
     end
 
     if config.autoresize then
-        vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+        local previous_win_id = 0
+
+        vim.api.nvim_create_autocmd('BufEnter', {
             group = augroup,
             callback = function(_)
                 -- This shouldn't be required with WinScrolled rewrite of 0.9
                 if not vim.fn.has('nvim-0.9') then
                     vim.api.nvim_exec_autocmds('WinScrolled', {})
                 end
+
                 require('focus').resize()
             end,
-            desc = 'Resize window',
+            desc = 'Resize splits',
+        })
+        vim.api.nvim_create_autocmd('WinEnter', {
+            group = augroup,
+            callback = function(_)
+                local current_win_id = vim.api.nvim_get_current_win()
+
+                -- If we have a horizontal split, center the window so we keep
+                -- the current line in view.
+                if previous_win_id == 0 then
+                    return
+                end
+
+                local cur_win_pos = vim.fn.win_screenpos(current_win_id)
+                local prev_win_pos = vim.fn.win_screenpos(previous_win_id)
+
+                -- If we switch between horizontal splits, center the window
+                if cur_win_pos[2] == prev_win_pos[2] then
+                    vim.api.nvim_win_call(previous_win_id, function()
+                        vim.api.nvim_command('normal! zz')
+                    end)
+                end
+            end,
+            desc = 'Center window of previous horizontal split',
+        })
+        vim.api.nvim_create_autocmd('WinLeave', {
+            group = augroup,
+            callback = function(_)
+                -- Remember the previous window id
+                previous_win_id = vim.api.nvim_get_current_win()
+            end,
+            desc = 'Save previous window id from split',
+        })
+        vim.api.nvim_create_autocmd('WinClosed', {
+            group = augroup,
+            callback = function(_)
+                previous_win_id = 0
+            end,
+            desc = 'Reset previous window id from closed split',
         })
     end
 
